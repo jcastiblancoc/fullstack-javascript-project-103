cdeb645f0faf3dcdff5e39d76d894e6aa61c12fc
jcastiblancoc <castiblanco.jeisson@gmail.com>
2025-03-07 21:36:15 -0500

    Fix code smell

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import parseFile from './parser.js';
 
 /**
@@ -10,20 +11,19 @@ const genDiff = (filepath1, filepath2) => {
     const data1 = parseFile(filepath1);
     const data2 = parseFile(filepath2);
 
-    // Obtener claves únicas ordenadas sin depender de Lodash
-    const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort();
+    const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2))); // Obtener claves ordenadas
 
-    // Generar diferencias
     const diff = keys.map((key) => {
-        const hasKey1 = Object.hasOwn(data1, key);
-        const hasKey2 = Object.hasOwn(data2, key);
-
-        if (!hasKey2) return `  - ${key}: ${data1[key]}`; // Solo en el primer archivo
-        if (!hasKey1) return `  + ${key}: ${data2[key]}`; // Solo en el segundo archivo
-        if (data1[key] !== data2[key]) {
-            return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`; // Diferente en ambos
+        if (!Object.hasOwn(data2, key)) {
+            return `  - ${key}: ${data1[key]}`; // Clave solo en el primer archivo
         }
-        return `    ${key}: ${data1[key]}`; // Igual en ambos
+        if (!Object.hasOwn(data1, key)) {
+            return `  + ${key}: ${data2[key]}`; // Clave solo en el segundo archivo
+        }
+        if (data1[key] !== data2[key]) {
+            return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`; // Clave con valores diferentes
+        }
+        return `    ${key}: ${data1[key]}`; // Clave con el mismo valor en ambos archivos
     });
 
     return `{\n${diff.join('\n')}\n}`;
